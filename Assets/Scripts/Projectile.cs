7d47dc7ca708b088ac1d433e748e927be840c58c
EPaz0 <enrique_pazg@yahoo.com>
2024-12-04 18:03:39 -0800

    Added impact vfx to fireball

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     private bool collided;
+    public GameObject impactVX;
     void OnCollisionEnter(Collision co) 
     {
         if (co.gameObject.tag != "Bullet" && co.gameObject.tag != "Player" && !collided) 
         {
             collided = true;
+
+            var impact = Instantiate(impactVX, co.contacts[0].point, Quaternion.identity) as GameObject;
+
+            Destroy(impact, 2f);
             Destroy(gameObject);
         }
         
